bb5b2cf8f6328c6d60aec619837657eafcbed159
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2017-10-21 18:24:11 +0530

    Step 7 - ApplicationConext container with Singleton

--- HelloWorld_ApplicationContext/src/test/Client.java
+++ HelloWorld_ApplicationContext/src/test/Client.java
@@ -1,8 +1,21 @@
 package test;
 
 
+import beans.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Client {
     public static void main(String [] args){
         System.out.println("Welcome to Java Spring framework on Application Context J2EE Container !!!!!");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources/spring.xml");
+        Test test = (Test) applicationContext.getBean("test");
+        System.out.println("-------------------------------------------");
+        Test test1 = (Test) applicationContext.getBean("test");
+        System.out.println("-------------------------------------------");
+        Test test2 = (Test) applicationContext.getBean("test");
+        System.out.println("-------------------------------------------");
+        test.sayHello();//Singleton//ByDefault its singleton
+
     }
 }
