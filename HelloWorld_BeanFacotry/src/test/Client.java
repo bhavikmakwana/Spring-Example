e76e3fb0e2b1760874f7441bc40f5862ab29527f
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2017-10-21 12:22:03 +0530

    Step-3 Basic HelloWorld from beanFactory Container or core container of ioc

--- HelloWorld_BeanFacotry/src/test/Client.java
+++ HelloWorld_BeanFacotry/src/test/Client.java
@@ -1,7 +1,20 @@
 package test;
 
+import beans.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class Client {
     public static void main(String [] args){
         System.out.println("Welcome to Java Spring with BeanFactory !!!");
+        //find the xml file for loading in ioc container
+        Resource resource = new ClassPathResource("resources/spring.xml");
+        //load xml file into container
+        BeanFactory factory = new XmlBeanFactory(resource);
+        //create Test class object
+        Test test = (Test) factory.getBean("test");//from ref name of POJO class which we gives in xml file
+        test.sayHello();
     }
 }
